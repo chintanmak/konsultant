00a5539446d9eefdcf4c6cf7aadd5954ec274dfa
Chintan Mak <chintanmakwana2011@gmail.com>
2023-05-01 01:07:16 +0530

    3rd commit

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <>
       <div className='header_container'>
-      <div className='header_section'>
         <img src={headerbg} />
         <div className='header_title'>
           <h2>A SUCCESSFUL STRATEGY FOR YOUR BUSINESS</h2>
@@ -16,13 +15,16 @@ export default function Home() {
           <button>VIEW CASE STUDIES</button>
         </div>
         </div>
-      </div>
 
       <div className='advice_container'>
         <div className='advice_texts'>
           <h2>YOU NEED THE BEST ADVICE</h2>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
           <button>READ MORE</button>
+        </div>
+        <div className='advice_img'>
+          <img src={personpic} />
         </div>
       </div>
     </>
